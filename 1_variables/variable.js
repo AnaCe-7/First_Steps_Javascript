36eeea519f04b1ce6a39bc64fcc553aadec5c160
AnaCe-7 <ana.cecilia.reques@gmail.com>
2023-11-21 00:08:40 +0100

    Exercises of variables in progress

--- 1_variables/variable.js
+++ 1_variables/variable.js
@@ -1,7 +1,36 @@
-// VARIBLES
+// Data type variables: numeric, text string, Boolean
 
-let year = 2023;
-let month = "November";
-let student = true;
+let number = 7;
+let animal = "gato";
+let isTrue = true;
 
+console.log(number);
+console.log(animal);
+console.log(isTrue);
 
+document.write();
+document.write();
+document.write();
+
+// Integer type variables and basic mathematical operations
+
+let sum = number + 100;
+let subtract = number - 3;
+let multiply = number * 2;
+let divide = number / 1;
+
+console.log("number + 100 = ", sum);
+console.log("number - 3 = ", subtract);
+console.log("number * 2 = ", multiply);
+console.log("number / 1 = ", divide);
+
+document.write();
+document.write();
+document.write();
+document.write();
+
+// Concatenation of text string variables
+
+console.log("My favorite animal is the " + animal);
+
+document.write("My favorite animal is the " + animal);
