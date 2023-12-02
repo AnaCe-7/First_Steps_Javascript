5c538353e329a3c6cbf9d52e9219ae7dcdf958cb
AnaCe-7 <ana.cecilia.reques@gmail.com>
2023-12-02 11:35:48 +0100

    improved sum function and array exercises done except the last one

--- 2_functions/functions.js
+++ 2_functions/functions.js
@@ -8,16 +8,14 @@ function greeting(){
 console.log(greeting());
 document.write(greeting());
 
-// 2) Sum function
-function sum(){
-    let number_a = 7;
-    let number_b = 3;
-    let result = number_a + number_b;
-    return "The sum of " + number_a + " plus " + number_b + " equals " + result;
-}
+// 2) Function sum of 2 parameters
+function sumOfNumbers(num1, num2) {
+    let result = num1 + num2;
+    console.log(`The sum of ${num1} + ${num1} equals ${result}`);
+    document.write(`<br>The sum of ${num1} + ${num1} equals ${result}`);
+};
 
-console.log(sum());
-document.write("<br>", sum());
+sumOfNumbers(7,7);
 
 // 3) Function of odd and even numbers
 function evenOrOdd(number) {
@@ -27,11 +25,9 @@ function evenOrOdd(number) {
         return (`The number ${number} is odd`);
     }
 };
-// Test the function in the console
-/*
+// Testing the function in the console
 console.log(evenOrOdd(10));
 console.log(evenOrOdd(7));
-*/
 
 // Display variable values on the browser screen
 document.write("<br>", evenOrOdd(20));
--- 4_arrays/arrays.js
+++ 4_arrays/arrays.js
@@ -6,9 +6,10 @@ document.write("This is an array of 10 numbers: " + arrayOfNumbers);
 
 // Function that prints in console all the numbers in an array
 function printConsoleArray(arrayOfNumbers) {
-    for (let i = 0; i < arrayOfNumbers; i++);
-    console.log(i);
+    for (let i = 0; i < arrayOfNumbers.length; i++) 
+    console.log(arrayOfNumbers[i]);
 };
+printConsoleArray(arrayOfNumbers)
 
 
 // Function that adds a number to an array
@@ -17,16 +18,16 @@ console.log(arrayOfNumbers);
 document.write("<br>Added a number to the array " + arrayOfNumbers);
 
 
-// // Function that removes the even numbers from an array
-// let showOnlyOdds = [];
-// function removeEven() {
-//     for (let i = 0; i < arrayOfNumbers; i++) {
-//         if (arrayOfNumbers[i] % 2 !== 0);
-//         showOnlyOdds.push(arrayOfNumbers[i]);
-//     }
-// };
-// console.log(showOnlyOdds);
-// document.write("<br>The odd numbers of the array are: " + showOnlyOdds);
+// Function that removes the even numbers from an array
+let showOnlyOdds = [];
+function removeEven() {
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        if (arrayOfNumbers[i] % 2 !== 0) showOnlyOdds.push(arrayOfNumbers[i]);
+    }
+};
+removeEven()
+console.log(showOnlyOdds);
+document.write("<br>The odd numbers of the array are: " + showOnlyOdds);
 
 
 // Function that returns the largest number in an array
@@ -49,7 +50,27 @@ console.log(smallestResult);
 document.write("<br>The smallest number in the array is: " + smallestResult);
 
 // Function that converts all the letters in a text to lower case
+let text1 = "An Apple A Day Keeps The Doctor Away";
+let lowerCaseText = text1.toLowerCase();
+console.log(lowerCaseText);
+document.write("<br>Lowercase texte: " + lowerCaseText);
 
 // Function that converts all the letters in a text to uppercase letters
+let text2 = "i have learned something new today";
+let upperCaseText = text2.toUpperCase();
+console.log(upperCaseText);
+document.write("<br>Uppercase texte: " + upperCaseText);
 
 // Function that converts the first letter of each element of the array to uppercase
+
+// let arrayNames = ["cecilia", "sebastian", "elodie"];
+// let firstLetterCapitalized = [];
+// function capitalize(arrayNames) {
+//     return arrayNames.map(word => {
+//         let firstLetter = word.charAt(0).toUpperCase();
+//         let remainder = word.slice(1).toLowerCase();
+
+//         return firstLetter + remainder;
+//     });
+// };
+//  console.log(capitalize([]));
