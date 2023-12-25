39a5f122ecd4a6b90364218bd5cd55d6bb647d8c
AnaCe-7 <ana.cecilia.reques@gmail.com>
2023-12-25 23:38:40 +0100

    1/11 DOM exercice done (button-display message)

--- 5_dom/dom.js
+++ 5_dom/dom.js
@@ -1,21 +1,26 @@
-// Display a message when a button is clicked
+// 1) Display a message when clicking on a button
+let button = document.getElementById('btn');
 
-// Change the content of an HTML element
+button.addEventListener('click', () => {
+    alert("Sorry, at the moment this button leads nowhere");
+})
 
-// Hide and show HTML elements
+// 2) Change the content of an HTML element
 
-// 
+// 3) Hide and display HTML elements
 
-//
+// 4) Array of 10 names
 
-//
+// 5) Function that prints on screen a list of names from the array of names
 
-//
+// 6) Array of numbers
 
-//
+// 7) Function that prints on screen how many numbers the array of numbers has
 
-//
+// 8) Nodes to print a form
 
-//
+// 9) Nodes to print a table
 
-//
+// 10) Array of 10 objects with their respective key:value
+
+// 11) Function that prints on screen each element of the object created above inside the table
