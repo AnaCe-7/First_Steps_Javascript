fe224d45f0e786765647552ec64f1e9f2224284d
AnaCe-7 <ana.cecilia.reques@gmail.com>
2023-12-25 17:18:42 +0100

    Changed text to display on browser screen

--- 3_objects/objects.js
+++ 3_objects/objects.js
@@ -6,6 +6,8 @@ let car = {
     parts: ["seats", "wheels", "lights"],
 };
 
+console.log(car);
+
 // Function that returns the car brand
 console.log("The brand of the car is: " + car.brand);
 document.write("<br>The brand of the car is: " + car.brand);
@@ -16,4 +18,4 @@ document.write("<br>This car has " + car.number_of_doors + " doors");
 
 // Function that returns an attribute of the nested object
 console.log("A part of the car is: " + car.parts[1]);
-document.write("<br>A part of the car is: " + car.parts[1]);
+document.write("<br>AOne of the parts of the car are the: " + car.parts[1]);
