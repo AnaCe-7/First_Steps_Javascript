76294b9eac37a49cbac38e17bb03ca83e2a5d000
AnaCe-7 <ana.cecilia.reques@gmail.com>
2023-12-25 16:02:02 +0100

    Non important changes in variables.js file

--- 1_variables/variable.js
+++ 1_variables/variable.js
@@ -1,4 +1,4 @@
-// 1) Data type variables , text string, Boolean
+// 1) Data type variables: numbers, text string, boolean
 let number = 7;
 let number_a = 100;
 let number_b = 133;
@@ -13,7 +13,7 @@ console.log(bool);
 
 // Display variable values on the browser screen
 document.write("The week has " + number + " days");
-document.write("<br>I love animals, but " + str + animal);
+document.write("<br>I love animals, and " + str + animal);
 document.write("<br>Boolean variables have two possible values: false and " + bool);
 
 // 2) Integer type variables and basic mathematical operations
