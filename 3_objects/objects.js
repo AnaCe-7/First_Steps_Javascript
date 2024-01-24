5d5d9cb34b8cf2581b25ec1aa7ac29955d5709b4
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-01-24 18:28:55 +0100

    Added line breaks <br> between exercises

--- 3_objects/objects.js
+++ 3_objects/objects.js
@@ -10,11 +10,11 @@ console.log(car);
 
 // Function that returns the car brand
 console.log("The brand of the car is: " + car.brand);
-document.write("<br>The brand of the car is: " + car.brand);
+document.write("<br>The brand of the car is: " + car.brand + "<br>");
 
 // Function that returns the number of doors
 console.log("This car has " + car.number_of_doors + " doors");
-document.write("<br>This car has " + car.number_of_doors + " doors");
+document.write("<br>This car has " + car.number_of_doors + " doors" + "<br>");
 
 // Function that returns an attribute of the nested object
 console.log("A part of the car is: " + car.parts[1]);
