14d325f815ce8fcde7da99fe0e61c03e93b1b888
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-01-24 18:32:48 +0100

    Added line breaks <br> between exercises

--- 4_arrays/arrays.js
+++ 4_arrays/arrays.js
@@ -1,7 +1,7 @@
 // 1) Array of 10 numbers
 let arrayOfNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(arrayOfNumbers);
-document.write("This is an array of 10 numbers: " + arrayOfNumbers);
+document.write("This is an array of 10 numbers: " + arrayOfNumbers + "<br>");
 
 
 // 2) Function that prints in console all the numbers in an array
@@ -15,7 +15,7 @@ printConsoleArray(arrayOfNumbers)
 // 3) Function that adds a number to an array
 arrayOfNumbers.push(10);
 console.log(arrayOfNumbers);
-document.write("<br>Added a number to the array " + arrayOfNumbers);
+document.write("<br>Added a number to the array " + arrayOfNumbers + "<br>");
 
 
 // 4) Function that removes the even numbers from an array
@@ -27,7 +27,7 @@ function removeEven() {
 };
 removeEven()
 console.log(showOnlyOdds);
-document.write("<br>The odd numbers of the array are: " + showOnlyOdds);
+document.write("<br>The odd numbers of the array are: " + showOnlyOdds + "<br>");
 
 
 // 5) Function that returns the largest number in an array
@@ -39,7 +39,7 @@ function largestNumber(arrayOfNumbers) {
 
 let largestResult = largestNumber(arrayOfNumbers);
 console.log(largestResult);
-document.write("<br>The largest number in the array is: " + largestResult);
+document.write("<br>The largest number in the array is: " + largestResult + "<br>");
 
 // 5.a) In this case the input parameter are simple list of numbers:
 console.log("The largest number in this other array is " + Math.max(9, 5.25, 3, -1, 0, -2));
@@ -54,21 +54,21 @@ function smallestNumber(arrayOfNumbers) {
 
 let smallestResult = smallestNumber(arrayOfNumbers);
 console.log(smallestResult);
-document.write("<br>The smallest number in the array is: " + smallestResult);
+document.write("<br>The smallest number in the array is: " + smallestResult + "<br>");
 
 
 // 7) Function that converts all the letters in a text to lowercase
 let text1 = "An Apple A Day KEEPS The Doctor AWAY";
 let lowerCaseText = text1.toLowerCase();
 console.log(lowerCaseText);
-document.write("<br>Lowercase texte: " + lowerCaseText);
+document.write("<br>Lowercase texte: " + lowerCaseText + "<br>");
 
 
 // 8) Function that converts all the letters in a text to uppercase
 let text2 = "i have learned something new today";
 let upperCaseText = text2.toUpperCase();
 console.log(upperCaseText);
-document.write("<br>Uppercase texte: " + upperCaseText);
+document.write("<br>Uppercase texte: " + upperCaseText + "<br>");
 
 
 // 9) Function that converts the first letter of each element of the array to uppercase
@@ -83,4 +83,4 @@ function capitalizeFirstLetter(array) {
 
 capitalizeFirstLetter(arrayNames);
 console.log(arrayNames);
-document.write("<br>Quino's best known work was Mafalda. The main characters of this famous comic strip are: " + arrayNames);
+document.write("<br>Quino's best known work was Mafalda. The main characters of this famous comic strip are: " + arrayNames + "<br>");
